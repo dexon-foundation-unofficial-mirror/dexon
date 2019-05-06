a7318ebed0c89a3312c3cca8fc2c6b6bc91b6228
Sonic <sonic@dexon.org>
2019-05-06 15:32:26 +0800

    fixup! les: rename package to lds

--- lds/freeclient_test.go
+++ lds/freeclient_test.go
@@ -14,8 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-// Package light implements on-demand retrieval capable state and chain objects
-// for the Ethereum Light Client.
+// Package lds implements the Light Dexon Subprotocol.
 package lds
 
 import (
